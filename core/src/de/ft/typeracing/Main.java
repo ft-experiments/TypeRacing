1b990d125f8bb0506f99cf41619622ab9eabc197
Coder246 <tim-arnold@outlook.de>
2020-05-27 18:20:39 +0200

    Variablen

--- core/src/de/ft/typeracing/Main.java
+++ core/src/de/ft/typeracing/Main.java
@@ -1,8 +1,6 @@
 package de.ft.typeracing;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,15 +8,67 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	long timelastpressed = 0;
+	long timepressaverage =0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		Gdx.app.setLogLevel(Application.LOG_INFO);
+
+		Gdx.input.setInputProcessor(new InputProcessor() {
+			@Override
+			public boolean keyDown(int keycode) {
+
+				return false;
+			}
+
+			@Override
+			public boolean keyUp(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyTyped(char character) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				return false;
+			}
+
+			@Override
+			public boolean mouseMoved(int screenX, int screenY) {
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(int amount) {
+				return false;
+			}
+		});
 	}
 
 	@Override
 	public void render () {
+
+
+
+
+
 
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
