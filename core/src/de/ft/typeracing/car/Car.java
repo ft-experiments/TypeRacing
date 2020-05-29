95ca83e12eeecbf2c25fd9d04a7dfd6265d7d660
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-05-29 13:24:14 +0200

    Update Car.java

--- core/src/de/ft/typeracing/car/Car.java
+++ core/src/de/ft/typeracing/car/Car.java
@@ -1,10 +1,15 @@
 package de.ft.typeracing.car;
 
+import com.badlogic.gdx.graphics.Texture;
+import de.ft.typeracing.Texte;
+
 public class Car {
     CarSteuerung carSteuerung =null;
+    Texture img_car;
 
-    public Car(CarSteuerung carSteuerung){
+    public Car(CarSteuerung carSteuerung, Texture img_car){
         this.carSteuerung=carSteuerung;
+
     }
 
     public CarSteuerung getCarSteuerung() {
